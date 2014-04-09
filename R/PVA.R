8f9474b51cfc1ac379e0b1897f7ba76dc5d3f79a
Bruce Kendall <kendall@bren.ucsb.edu>
2014-04-08 17:11:58 -0700

    Clean up variable names, add error checking.

--- R/PVA.R
+++ R/PVA.R
@@ -7,23 +7,28 @@
 #' @param mean mean of the distribution (must be in (0,1))
 #' @param sdev standard deviation of the distribution. Limits depend on the value of \code{mean}
 #' 
+#' @details All arguments must be scalars.
 #' @return a vector of length \code{n} with the random numbers
 #' @seealso \code{\link{rbeta}} and \code{\link{betaval}}
 rbeta2 <- function(n, mean, sdev) {
+    stopifnot(is.numeric(n), is.numeric(mean), is.numeric(sdev), 
+              length(n) == 1, length(mean) ==1, length(sdev) == 1)
+  
   if (mean > 1 || mean < 0) { # Ensure parameter is within bounds
-    stop("Please select a mean beta between 0 and 1")
-  }
-  if (sdev == 0) { # No randomness
-    bb <- rep(mean, n) 
+    stop("Attempted to use a mean for the beta distribution that is outside of [0,1]")
+  } else if (sdev == 0) { # No randomness - return n copies of mean
+    warning("Requesting random variates with no variation. Returning replicates of mean")
+    return(rep(mean, n))
   } else {
-    vari <- sdev^2
-    if (vari >= (1 - mean) * mean) { # Ensure parameter is within bounds
+    sigma2 <- sdev^2
+    if (sigma2 >= (1 - mean) * mean) { # Ensure parameter is within bounds
       stop("Standard deviation too high for beta distribution")
     }
     # Convert mean and variance to shape parameters
-    vv <- mean * ((mean * (1 - mean)/(vari)) - 1)
-    ww <- (1 - mean) * ((mean * (1 - mean)/(vari)) - 1)
+    temp <- ((mean * (1 - mean)/(sigma2)) - 1)
+    a <- mean * temp
+    b <- (1 - mean) * temp
     # Get the random variates
-    bb <- rbeta(n, vv, ww)
+    return(rbeta(n, a, b))
   }
 }
