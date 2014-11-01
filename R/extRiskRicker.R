f7f5092e26adff6c7ab26ceb14b5ab10dd39250a
Bruce Kendall <kendall@bren.ucsb.edu>
2014-11-01 15:34:14 -0700

    New functions for simulating and calculating extinction CDF for Ricker model

---
+++ R/extRiskRicker.R
@@ -0,0 +1,27 @@
+#' Calculate a CDF of quasi-extinction risk for the Ricker model
+#' @param r the low-density growth rate
+#' @param K the equilibrium density
+#' @param sigma2 the variance of the log growth rates
+#' @param time_horizon number of years to simulate
+#' @param N0 initial population size
+#' @param nsims number of simulations
+#' @param Nx quasi-extinction threshold
+#' @param d the difference between \code{log(N0)} and \code{log(Nx)}. Either specify d or specify Nx
+#'   
+#' @return A data frame with columns \code{Time} and \code{CDF}
+extRiskRicker <- function(r, K, sigma2, time_horizon, N0, nsims=1000, Nx=NULL, d=log(N0/Nx) ) {
+  if (length(d)==0) {
+    stop("Either specify Nx or specify d")
+  }
+  if (is.null(Nx)) {
+    Nx <- N0/exp(d)
+  }
+  if (Nx >= N0) {
+    stop("Nx must be less than N0 (or d must be positive)")
+  }
+  replicated_simulations <- simulateRicker(r, K, sigma2, N0, time_horizon, nsims)
+  quasi_extinctions <- replicated_simulations < Nx
+  cumulative_quasi_extinctions <- apply(quasi_extinctions, 2, cummax)
+  quasi_extinction_CDF <- apply(cumulative_quasi_extinctions, 1, mean)
+  return(data.frame(Time=1:time_horizon, CDF=quasi_extinction_CDF))
+}
---
+++ R/simulateRicker.R
@@ -0,0 +1,46 @@
+#' Simulate one or more time series from the stochastic Ricker model
+#' @param r the low-density growth rate
+#' @param K the equilibrium density
+#' @param sigma2 the variance of the log growth rates
+#' @param N0 initial population size
+#' @param time_horizon number of years to simulate
+#' @param nsims number of simulations
+#' @param keep_N0 (logical) whether to return \code{N0} at the beginning of the time series
+#' 
+#' @return If \code{nsim=1} (the default), a vector of length \code{time_horizon} (or \code{time_horizon+1} if \code{keep_N0=TRUE}) with the simulated abundances. Otherwise, a matrix with \code{time_horizon} (or \code{time_horizon+1} if \code{keep_N0=TRUE}) rows and \code{nsim} columns.
+#' 
+#' @details This function simulates the Ricker discrete time model with environmental stochasticity, defined as follows
+#' \deqn{N(t+1) = N(t) exp[r (1 - N(t)/K) + \epsilon(t)]}, 
+#' where
+#' \deqn{\epsilon(t) ~ Normal(0, \sigma)}
+#' 
+#' @examples
+#' simulateRicker(2, 100, 0.01, 20, 10)
+#' simulateRicker(2, 100, 0.01, 20, 10, 5, keep_N0=TRUE)
+simulateRicker <- function (r, K, sigma2, N0, time_horizon, nsims=1, keep_N0=FALSE) {
+  sigma <- sqrt(sigma2)
+  # We keep track of both abundance and log abundance to reduce the number
+  #   of transendental function evaluations
+  Nt <- matrix(0, time_horizon+1, nsims)
+  Xt <- matrix(0, time_horizon+1, nsims)
+  Nt[1,] <- N0
+  Xt[1,] <- log(N0)
+  
+  # Pre-calculate the epsilons for efficiency
+  epsilont <- matrix(rnorm(time_horizon*nsims, 0, sigma), time_horizon, nsims)
+  
+  for (Time in 1:time_horizon) {
+    Xt[Time+1,] <- Xt[Time,] + r * (1 - Nt[Time,]/K + epsilont[Time,])
+    Nt[Time+1,] <- exp(Xt[Time+1,])
+  }
+  
+  if (!keep_N0) { # Drop the first row
+    Nt <- Nt[-1,]
+  }
+  
+  if (nsims==1) { # Return a vector
+    as.vector(Nt)
+  } else { # Return the matrix
+    Nt
+  }
+}
